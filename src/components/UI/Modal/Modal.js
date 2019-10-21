28d495d6209f935a4fbc0032b9ba224234a331a3
dtonal <dtonal@posteo.de>
2019-10-21 17:33:13 +0200

    Improve rendering of modal children

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop'
 
+class Modal extends Component{
 
-const modal = (props) => (
-    <>
-        <Backdrop show={props.show} backHandler={props.backHandler}/>
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show != this.props.show;
+    }
+
+    render = () => {
+        return <>
+        <Backdrop show={this.props.show} backHandler={this.props.backHandler}/>
         <div className={classes.Modal}
             style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(+100vh)',
-                opacity: props.show ? '1' : '0'
+                transform: this.props.show ? 'translateY(0)' : 'translateY(+100vh)',
+                opacity: this.props.show ? '1' : '0'
             }}
             >
-            {props.children}
+            {this.props.children}
         </div>
     </>
-);
+    }
+}
 
-export default modal;
+export default Modal;
